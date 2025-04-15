253d38ab30b11d7a4c90b6ed183efac4874b6721
Carlota Joana Mendes Lemos <1241330@isep.ipp.pt>
2025-04-15 21:03:19 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
     output: 'export',
+    images: {
+        unoptimized: true, // se estiveres a usar <Image />
+    },
 };
-
-module.exports = nextConfig;
